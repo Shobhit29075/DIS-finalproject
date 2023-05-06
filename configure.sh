59629cdb150e83c5cee13c3ce3da8385ee4d4d25
Shobhit29075 <68530260+Shobhit29075@users.noreply.github.com>
2023-05-06 05:00:13 -0400

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get install git
+yes '' | sudo apt-get install python3
+yes '' | sudo apt-get install nginx
+yes '' | sudo apt-get install python3-pip
+sudo pip3 install flask
+yes '' | sudo apt-get install gunicorn3 
+sudo pip3 install boto3
+git clone https://github.com/Shobhit29075/DIS-finalproject.git
+cd DIS-finalproject
+sudo cp flaskapp /etc/nginx/sites-enabled/
+cd /etc/nginx/sites-enabled/
+sudo service nginx restart
+cd ~
+cd DIS-finalproject
+yes '' | nohup gunicorn3 app:app &
