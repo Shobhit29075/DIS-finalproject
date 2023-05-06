935c7158639d8fc4156a6705a5f3f8ba58f86278
Shobhit29075 <68530260+Shobhit29075@users.noreply.github.com>
2023-05-06 03:13:32 -0400

    Add files via upload
    Adding files

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template
+import boto3
+app=Flask(__name__)
+@app.route("/")
+def homepage():
+    return render_template('form.html')
+
+@app.route("/login")
+def login():
+    user = request.values.get("user")
+    # getting input with name = lname in HTML form
+    pwd = request.values.get("pwd")
+    # print("-"+user+"-")
+    # print("-"+pwd+"-")
+
+    client = boto3.client('dynamodb',
+   
+    aws_access_key_id="AKIAU26FNIUZVIPM4GYN",
+    aws_secret_access_key="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",
+    region_name="us-east-1")
+    response=client.get_item(
+        TableName='Authentication',
+        Key={
+            'username': {'S':str(user)},
+            'password': {'S':str(pwd)}
+        }
+    )
+    # print(response)
+    if 'Item' in response.keys():
+        return "<h1>Welcome "+user+"</h1>"
+    else:
+        return "INVALID CREDENTIALS"
+
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=8080)
