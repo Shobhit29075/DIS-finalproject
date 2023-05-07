f26a233a787bf2596d1f520cd81a140664ef0c04
Shobhit29075 <68530260+Shobhit29075@users.noreply.github.com>
2023-05-07 12:18:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def login():
 
     client = boto3.client('dynamodb',
    
-
+    aws_access_key_id="AKIAU26FNIUZVIPM4GYN",	
+	aws_secret_access_key="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",
     region_name="us-east-1")
     response=client.get_item(
         TableName='Authentication',
