4db248bb534d985fe5373a5c714929a3e658a3e7
Shobhit29075 <68530260+Shobhit29075@users.noreply.github.com>
2023-05-07 12:12:52 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,9 +8,11 @@ yes '' | sudo apt-get install gunicorn3
 sudo pip3 install boto3
 git clone https://github.com/Shobhit29075/DIS-finalproject.git
 cd DIS-finalproject
+sudo chown "$(whoami)" flaskapp
+python3 change_flaskapp.py
 sudo cp flaskapp /etc/nginx/sites-enabled/
 cd /etc/nginx/sites-enabled/
 sudo service nginx restart
-cd /
+cd ~
 cd DIS-finalproject
 yes '' | nohup gunicorn3 app:app &
